2dbd9f39f2c669ed8edd3ad4302125f469e271aa
jyoti kumari <jk648892@gmail.com>
2023-08-14 16:03:29 +0530

    adding Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -1,8 +1,9 @@
 public class Multiply{
     public static void main(String[] args){
-        int a=10;
-        int b=8;
+        java.util.Scanner sc=new java.util.Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
         System.out.println("Multiply of a and b:"+a*b);
-        
+
     }
 }
