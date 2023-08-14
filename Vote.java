5c6b9fb415c3d871129f5884bf4d1b6a447f480c
jyoti kumari <jk648892@gmail.com>
2023-08-14 15:14:41 +0530

    initial commit

---
+++ Vote.java
@@ -0,0 +1,14 @@
+public class Vote {
+    public static void main(String[] args){
+        java.util.Scanner sc=new java.util.Scanner(System.in);
+        System.out.println("Enter age:")
+        int age=sc.nextInt();
+        if(age>18){
+            System.out.println("you are eligible for Vote");
+        }
+        else{
+            System.out.println("Not eligible");
+        }
+        
+    }
+}
